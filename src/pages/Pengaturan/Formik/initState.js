1605eda39e1c0eea3275fa7b5b1a7b6aeebd4443
Nova Dwi Sapta Nain Seven <novadwisaptans@gmail.com>
2022-01-14 22:12:52 +0800

    Update

--- src/pages/Pengaturan/Formik/initState.js
+++ src/pages/Pengaturan/Formik/initState.js
@@ -1,9 +1,8 @@
 const initState = (data) => {
   return {
     title_website: data ? data.title_website : "",
-    title_website: data ? data.title_website : "",
     deskripsi_aplikasi: data ? data.deskripsi_aplikasi : "",
-    deskripsi_aplikasi: "",
+    logo: "",
   };
 };
 
--- src/pages/Pengaturan/index.js
+++ src/pages/Pengaturan/index.js
@@ -7,16 +7,23 @@ import {
   Input,
   Label,
   Textarea,
+  HelperText,
 } from "@windmill/react-ui";
 import { GlobalContext } from "../../context/Provider";
-import { getKonten } from "../../context/actions/Konten";
+import { editKonten, getKonten } from "../../context/actions/Konten";
+import getImage from "../../context/actions/Files/getImage";
 import { Formik } from "formik";
+import initState from "./Formik/initState";
+import validationSchema from "./Formik/validationSchema";
+import { useHistory } from "react-router-dom";
 
 const Pengaturan = () => {
+  const history = useHistory();
   const [selectedFile, setSelectedFile] = useState();
   const [preview, setPreview] = useState();
   const { kontenState, kontenDispatch } = useContext(GlobalContext);
-  const { loading, dataKonten: data } = kontenState;
+  const [loading, setLoading] = useState(false);
+  const { data: dataKonten } = kontenState;
 
   // Get data kontent
   useEffect(() => {
@@ -25,8 +32,10 @@ const Pengaturan = () => {
 
   // Menangani preview input gambar setelah dipilih
   const handleSelectedFile = useCallback(() => {
+    const logo2 = dataKonten ? getImage("", dataKonten.logo) : null;
+
     if (!selectedFile) {
-      setPreview(null);
+      setPreview(logo2);
       return;
     }
 
@@ -37,7 +46,7 @@ const Pengaturan = () => {
     return () => {
       URL.revokeObjectURL(objectUrl);
     };
-  }, [selectedFile]);
+  }, [selectedFile, dataKonten]);
 
   const onSelectFile = (e) => {
     if (!e.target.files || e.target.files.length === 0) {
@@ -53,7 +62,15 @@ const Pengaturan = () => {
   }, [handleSelectedFile]);
 
   const handleFormSubmit = (values) => {
-    alert(JSON.stringify(values));
+    // alert(JSON.stringify(values));
+    const formData = new FormData();
+    formData.append("title_website", values.title_website);
+    formData.append("deskripsi_aplikasi", values.deskripsi_aplikasi);
+    if (values.logo) {
+      formData.append("logo", values.logo);
+    }
+
+    editKonten(formData, setLoading, history, kontenDispatch);
   };
 
   return (
@@ -63,7 +80,7 @@ const Pengaturan = () => {
       <Card className="mb-32">
         <CardBody>
           <Formik
-            initialValues={initState}
+            initialValues={initState(dataKonten)}
             validationSchema={validationSchema}
             onSubmit={handleFormSubmit}
             enableReinitialize
@@ -86,25 +103,65 @@ const Pengaturan = () => {
                     <span>Nama Aplikasi</span>
                     <Input
                       type="text"
-                      className="mt-1"
                       placeholder="Nama Aplikasi"
+                      name="title_website"
+                      value={values.title_website}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className={`mt-1 ${
+                        errors.title_website && touched.title_website
+                          ? "border-red-500"
+                          : null
+                      }`}
                     />
+                    {errors.title_website && touched.title_website && (
+                      <HelperText valid={false}>
+                        {errors.title_website}
+                      </HelperText>
+                    )}
                   </Label>
                   <Label className="mt-4">
                     <span>Deskripsi Aplikasi</span>
                     <Textarea
-                      className="mt-1"
                       rows={3}
                       placeholder="Deskripsi Aplikasi"
+                      name="deskripsi_aplikasi"
+                      value={values.deskripsi_aplikasi}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className={`mt-1 ${
+                        errors.deskripsi_aplikasi && touched.deskripsi_aplikasi
+                          ? "border-red-500"
+                          : null
+                      }`}
                     />
+                    {errors.deskripsi_aplikasi &&
+                      touched.deskripsi_aplikasi && (
+                        <HelperText valid={false}>
+                          {errors.deskripsi_aplikasi}
+                        </HelperText>
+                      )}
                   </Label>
                   <Label className="mt-4">
                     <span>Logo</span>
                     <Input
                       type="file"
-                      className="mt-1 mb-2"
-                      onChange={(e) => onSelectFile(e)}
+                      className="mt-4 mb-3"
+                      onChange={(e) => {
+                        onSelectFile(e);
+                        setFieldValue("logo", e.target.files[0]);
+                      }}
+                      onBlur={handleBlur}
+                      name="logo"
+                      className={`mt-1 ${
+                        errors.logo && touched.logo ? "border-red-500" : null
+                      }`}
                     />
+                    {errors.logo && touched.logo && (
+                      <HelperText valid={false}>
+                        {errors.logo} <br />
+                      </HelperText>
+                    )}
                     {preview && (
                       <img
                         src={preview}
@@ -119,8 +176,15 @@ const Pengaturan = () => {
                     </span>
                   </Label>
                   <div className="mt-5 flex flex-col-reverse md:flex-row justify-end gap-2">
-                    <Button layout="outline">Reset</Button>
-                    <Button>Simpan</Button>
+                    <Button onClick={handleReset} layout="outline">
+                      Reset
+                    </Button>
+                    <Button
+                      type="submit"
+                      disabled={loading || !isValid ? true : false}
+                    >
+                      {loading ? "Loading" : "Simpan"}
+                    </Button>
                   </div>
                 </div>
               </form>
