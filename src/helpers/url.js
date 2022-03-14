beeb08ab30471fdab0a9d6c7789cbf2c7edc574c
Nova Dwi Sapta Nain Seven <novadwisaptans@gmail.com>
2022-03-14 15:40:19 +0800

    Update

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,3 +1,9 @@
-export const baseURL = "http://localhost/kpn_bandar_segara_api/api/";
+// LOCALHOST
+// export const baseURL = "http://localhost/kpn_bandar_segara_api/api/";
+// export const baseUrlImg =
+//   "http://localhost/kpn_bandar_segara_api/public/images/";
+
+// PUBLIC
+export const baseURL = "https://kpnbandarsegara.com/kpn_bandar_segara_api/api/";
 export const baseUrlImg =
-  "http://localhost/kpn_bandar_segara_api/public/images/";
+  "https://kpnbandarsegara.com/kpn_bandar_segara_api/public/images/";
